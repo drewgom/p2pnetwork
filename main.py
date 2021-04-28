ef79451e6872dcbfc4c2a18ac93a74db2f871339
Drew Gomez <andrew.gomez01@student.csulb.edu>
2021-04-28 10:17:06 -0700

    adding main.py's changes for the previous commit

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import net_scan
+import p2p_conn
+import sys
 
-ip_arr = net_scan.scan_network()
+
+if sys.argv[1] == 'sender':
+	ip_arr = []
+	ip_arr.append("192.168.1.7")
+	print("starting port scan")
+	result = net_scan.scan_ports(ip_arr, 2195)
+	print("port scan completed")
+	for res in result:
+		p2p_conn.start_sender(res)
+elif sys.argv[1] == 'receiver':
+	p2p_conn.start_receiver()
+else:
+	print('Unknown argument')
